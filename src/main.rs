93d8cb2e79c8cc74d86d3ae12bac9ea604e9ca53
wesley <wesley@bitechular.com>
2024-10-18 23:09:45 +0200

    Working 2 streams

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use anyhow::Error;
 use gstreamer as gst;
-use gstreamer::prelude::{ElementExt, GstBinExtManual, GstObjectExt, ObjectExt, PadExt};
+use gstreamer::log;
+use gstreamer::prelude::{ElementExt, GstBinExt, GstBinExtManual, GstObjectExt, ObjectExt, PadExt};
 use gstreamer_rtsp::RTSPLowerTrans;
-use std::sync::{Arc, Mutex};
 
 fn main() -> Result<(), Error> {
     // Initialize GStreamer
@@ -11,86 +11,122 @@ fn main() -> Result<(), Error> {
     // Create the pipeline
     let pipeline = gst::Pipeline::new();
 
-    // Create elements
-    let rtspsrc = gst::ElementFactory::make_with_name("rtspsrc", Some("rtspsrc"))
-        .expect("Could not create rtspsrc element.");
-    let rtpmp4gdepay = gst::ElementFactory::make_with_name("rtpmp4gdepay", Some("rtpmp4gdepay"))
-        .expect("Could not create rtpmp4gdepay element.");
-    let aacparse = gst::ElementFactory::make_with_name("aacparse", Some("aacparse"))
-        .expect("Could not create aacparse element.");
-    let decodebin = gst::ElementFactory::make_with_name("decodebin", Some("decodebin"))
-        .expect("Could not create decodebin element.");
+    // Create elements for the first RTSP source (camera)
+    let rtspsrc1 = gst::ElementFactory::make_with_name("rtspsrc", Some("rtspsrc1"))
+        .expect("Could not create rtspsrc1 element.");
+    let rtpmp4gdepay1 = gst::ElementFactory::make_with_name("rtpmp4gdepay", Some("rtpmp4gdepay1"))
+        .expect("Could not create rtpmp4gdepay1 element.");
+    let aacparse1 = gst::ElementFactory::make_with_name("aacparse", Some("aacparse1"))
+        .expect("Could not create aacparse1 element.");
+    let decodebin1 = gst::ElementFactory::make_with_name("decodebin", Some("decodebin1"))
+        .expect("Could not create decodebin1 element.");
     let queue1 = gst::ElementFactory::make_with_name("queue", Some("queue1"))
-        .expect("Could not create queue element.");
+        .expect("Could not create queue1 element.");
     let audioconvert1 = gst::ElementFactory::make_with_name("audioconvert", Some("audioconvert1"))
-        .expect("Could not create audioconvert element.");
-    let audioresample = gst::ElementFactory::make_with_name("audioresample", Some("audioresample"))
-        .expect("Could not create audioresample element.");
+        .expect("Could not create audioconvert1 element.");
+    let audioresample1 = gst::ElementFactory::make_with_name("audioresample", Some("audioresample1"))
+        .expect("Could not create audioresample1 element.");
+
+    // Create elements for the second RTSP source (Spotify)
+    let rtspsrc2 = gst::ElementFactory::make_with_name("rtspsrc", Some("rtspsrc2"))
+        .expect("Could not create rtspsrc2 element.");
+    let rtpmp4gdepay2 = gst::ElementFactory::make_with_name("rtpmp4gdepay", Some("rtpmp4gdepay2"))
+        .expect("Could not create rtpmp4gdepay2 element.");
+    let aacparse2 = gst::ElementFactory::make_with_name("aacparse", Some("aacparse2"))
+        .expect("Could not create aacparse2 element.");
+    let decodebin2 = gst::ElementFactory::make_with_name("decodebin", Some("decodebin2"))
+        .expect("Could not create decodebin2 element.");
     let queue2 = gst::ElementFactory::make_with_name("queue", Some("queue2"))
-        .expect("Could not create queue element.");
+        .expect("Could not create queue2 element.");
+    let audioconvert2 = gst::ElementFactory::make_with_name("audioconvert", Some("audioconvert2"))
+        .expect("Could not create audioconvert2 element.");
+    let audioresample2 = gst::ElementFactory::make_with_name("audioresample", Some("audioresample2"))
+        .expect("Could not create audioresample2 element.");
+
+    // Common elements
     let audiomixer = gst::ElementFactory::make_with_name("audiomixer", Some("audiomixer"))
         .expect("Could not create audiomixer element.");
-    let audioconvert2 = gst::ElementFactory::make_with_name("audioconvert", Some("audioconvert2"))
-        .expect("Could not create audioconvert element.");
     let lamemp3enc = gst::ElementFactory::make_with_name("lamemp3enc", Some("lamemp3enc"))
         .expect("Could not create lamemp3enc element.");
     let rtspclientsink = gst::ElementFactory::make_with_name("rtspclientsink", Some("rtspclientsink"))
         .expect("Could not create rtspclientsink element.");
 
     // Set element properties
-    rtspsrc.set_property("location", &"rtsp://10.0.0.12:8554/camera.rlc_520a_clear");
-    rtspsrc.set_property("protocols", RTSPLowerTrans::TCP);
+    rtspsrc1.set_property("location", &"rtsp://10.0.0.12:8554/camera.rlc_520a_clear");
+    rtspsrc1.set_property("protocols", RTSPLowerTrans::TCP);
+    rtspsrc2.set_property("location", &"rtsp://10.0.0.21:8554/spotify");
+    rtspsrc2.set_property("protocols", RTSPLowerTrans::TCP);
     rtspclientsink.set_property("location", &"rtsp://localhost:8554/lumi");
     lamemp3enc.set_property("bitrate", &320);
 
+
     // Add elements to the pipeline
     pipeline.add_many(&[
-        &rtspsrc, &rtpmp4gdepay, &aacparse, &decodebin, &queue1, &audioconvert1, &audioresample,
-        &queue2, &audiomixer, &audioconvert2, &lamemp3enc, &rtspclientsink
+        &rtspsrc1, &rtpmp4gdepay1, &aacparse1, &decodebin1, &queue1, &audioconvert1, &audioresample1,
+        &rtspsrc2, &rtpmp4gdepay2, &aacparse2, &decodebin2, &queue2, &audioconvert2, &audioresample2,
+        &audiomixer, &lamemp3enc, &rtspclientsink
     ])?;
 
-    // Link static elements
+    // Link static elements for the first RTSP source
     gst::Element::link_many(&[
-        &rtpmp4gdepay, &aacparse, &decodebin
+        &rtpmp4gdepay1, &aacparse1, &decodebin1
+    ])?;
+    gst::Element::link_many(&[
+        &queue1, &audioconvert1, &audioresample1, &audiomixer
+    ])?;
+
+    // Link static elements for the second RTSP source
+    gst::Element::link_many(&[
+        &rtpmp4gdepay2, &aacparse2, &decodebin2
+    ])?;
+    gst::Element::link_many(&[
+        &queue2, &audioconvert2, &audioresample2, &audiomixer
     ])?;
 
     gst::Element::link_many(&[
-        &queue1, &audioconvert1, &audioresample, &queue2, &audiomixer,
-        &audioconvert2, &lamemp3enc, &rtspclientsink
+        &audiomixer, &lamemp3enc, &rtspclientsink
     ])?;
 
-    // Connect to the pad-added signal of the rtspsrc element to dynamically link its source pad
-    rtspsrc.connect_pad_added(move |src, src_pad| {
+    // Connect to the pad-added signal of the rtspsrc1 element to dynamically link its source pad
+    rtspsrc1.connect_pad_added(move | _src, src_pad| {
         let src_pad_caps = src_pad.current_caps().unwrap();
         let src_pad_structure = src_pad_caps.structure(0).unwrap();
 
-        // Get the "media" field from the caps
         if let Ok(media_type) = src_pad_structure.get::<&str>("media") {
             if media_type == "audio" {
-                let sink_pad = rtpmp4gdepay.static_pad("sink").expect("Failed to get sink pad");
-                let source_name = src_pad.name().to_string();
-                let sink_name = sink_pad.name().to_string();
-
-                println!("Connecting audio pads: {} -> {}", source_name, sink_name);
-
+                let sink_pad = rtpmp4gdepay1.static_pad("sink").expect("Failed to get sink pad");
                 if let Err(err) = src_pad.link(&sink_pad) {
-                    eprintln!("Failed to link audio pads {} -> {}: {}", source_name, sink_name, err);
+                    eprintln!("Failed to link rtspsrc1 audio: {}", err);
                 }
-
-                if sink_pad.is_linked() {
-                    println!("Audio pad is successfully linked: {}", sink_pad.name());
-                }
-            } else {
-                println!("Ignoring non-audio stream of type: {}", media_type);
             }
-        } else {
-            println!("Failed to get 'media' field from caps");
         }
     });
 
-    // Handle decodebin's dynamic pad linking
-    decodebin.connect_pad_added(move |_, src_pad| {
+    // Connect to the pad-added signal of the rtspsrc2 element to dynamically link its source pad
+    rtspsrc2.connect_pad_added(move | _src, src_pad| {
+        let src_pad_caps = src_pad.current_caps().unwrap();
+        let src_pad_structure = src_pad_caps.structure(0).unwrap();
+
+        if let Ok(media_type) = src_pad_structure.get::<&str>("media") {
+            if media_type == "audio" {
+                let sink_pad = rtpmp4gdepay2.static_pad("sink").expect("Failed to get sink pad");
+                if let Err(err) = src_pad.link(&sink_pad) {
+                    eprintln!("Failed to link rtspsrc2 audio: {}", err);
+                }
+            }
+        }
+    });
+
+    decodebin1.connect_pad_added(move |_, src_pad| {
         let queue1_sink_pad = queue1.static_pad("sink").expect("Failed to get sink pad from queue1");
+
+        if let Err(err) = src_pad.link(&queue1_sink_pad) {
+            eprintln!("Failed to link decodebin to queue1: {}", err);
+        }
+    });
+
+    decodebin2.connect_pad_added(move |_, src_pad| {
+        let queue1_sink_pad = queue2.static_pad("sink").expect("Failed to get sink pad from queue1");
 
         if let Err(err) = src_pad.link(&queue1_sink_pad) {
             eprintln!("Failed to link decodebin to queue1: {}", err);
