14732d98e89c472a82e80dc8fdfe0d350126cbb5
wesley <wesley@bitechular.com>
2024-10-23 09:39:25 +0200

    Refactor code, move pipeline & element configuration to separate file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+mod pipeline;
+
 use anyhow::Error;
 use gstreamer as gst;
 use gstreamer::prelude::*;
-use gstreamer::prelude::*;
-use gstreamer_rtsp::RTSPLowerTrans;
-use gstreamer_rtsp_server::prelude::RTSPMediaExt;
+use serde_json::Value;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 use tokio;
@@ -11,162 +11,20 @@ use tokio::process::Command;
 use tokio::time;
 use warp::http::StatusCode;
 use warp::Filter;
+use pipeline::StreamPipeline;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    gst::init()?;
+    let gst_pipeline = StreamPipeline::new()?;
 
-    let pipeline = gst::Pipeline::new();
-
-    // Create elements for the rtsp livestream source
-    let livestream_source = gst::ElementFactory::make_with_name("rtspsrc", Some("livestream_source"))
-        .expect("Could not create livestream_source element.");
-    let livestream_depay = gst::ElementFactory::make_with_name("rtpmp4gdepay", Some("livestream_depay"))
-        .expect("Could not create livestream_depay element.");
-    let livestream_parse = gst::ElementFactory::make_with_name("aacparse", Some("livestream_parse"))
-        .expect("Could not create livestream_parse element.");
-    let livestream_decoder = gst::ElementFactory::make_with_name("decodebin", Some("livestream_decoder"))
-        .expect("Could not create livestream_decoder element.");
-    let livestream_queue = gst::ElementFactory::make_with_name("queue", Some("livestream_queue"))
-        .expect("Could not create livestream_queue element.");
-    let livestream_convert = gst::ElementFactory::make_with_name("audioconvert", Some("livestream_convert"))
-        .expect("Could not create livestream_convert element.");
-    let livestream_resample = gst::ElementFactory::make_with_name("audioresample", Some("livestream_resample"))
-        .expect("Could not create livestream_resample element.");
-    let livestream_buffer = gst::ElementFactory::make_with_name("queue", Some("livestream_buffer"))
-        .expect("Could not create livestream_buffer element.");
-    let livestream_rgvolume = gst::ElementFactory::make_with_name("rgvolume", Some("livestream_rgvolume"))
-        .expect("Could not create livestream_rgvolume element.");
-    let livestream_dsp = gst::ElementFactory::make_with_name("webrtcdsp", Some("livestream_dsp"))
-        .expect("Could not create livestream_dsp element.");
-    let livestream_cap_filter = gst::ElementFactory::make_with_name("capsfilter", Some("livestream_cap_filter")).expect("Failed to create capsfilter");
-    let livestream_cap_resample = gst::ElementFactory::make_with_name("audioresample", Some("livestream_cap_resample"))
-        .expect("Could not create audioresample element for capsfilter");
-    let livestream_cap_convert = gst::ElementFactory::make_with_name("audioconvert", Some("capsfilter_converter"))
-        .expect("Could not create audioconvert element for capsfilter");
-
-    livestream_source.set_property("location", &"rtsp://10.0.0.12:8554/camera.rlc_520a_clear");
-    livestream_source.set_property("protocols", RTSPLowerTrans::TCP);
-
-    livestream_dsp.set_property("echo-cancel", &false);
-    livestream_dsp.set_property("noise-suppression", &true);
-    livestream_dsp.set_property_from_str("noise-suppression-level", &"high");
-    livestream_dsp.set_property("voice-detection", &true);
-    livestream_dsp.set_property("extended-filter", &true);
-
-    // Reduce volume
-    livestream_rgvolume.set_property("pre-amp", &-30.0f64);
-
-    livestream_buffer.set_property("max-size-buffers", &0u32);
-    livestream_buffer.set_property("max-size-bytes", &0u32);
-    livestream_buffer.set_property("max-size-time", &(300_000_000u64));
-
-    // Create elements for the music source
-    let music_source = gst::ElementFactory::make_with_name("pulsesrc", Some("music_source"))
-        .expect("Could not create music_source element.");
-    let music_volume = gst::ElementFactory::make_with_name("volume", Some("livestream_volume"))
-        .expect("Could not create livestream_volume element.");
-    let music_cap_filter = gst::ElementFactory::make_with_name("capsfilter", Some("music_cap_filter"))
-        .expect("Failed to create music_cap_filter");
-
-    music_source.set_property("device", &"alsa_output.platform-bcm2835_audio.analog-stereo.monitor");
-    music_volume.set_property("volume", 1.0f64);
-    music_cap_filter.set_property("caps", &gst::Caps::builder("audio/x-raw")
-        .field("format", &"S16LE")
-        .field("rate", &44100)
-        .field("channels", &2)
-        .build());
-
-    let music_buffer = gst::ElementFactory::make_with_name("queue", Some("music_buffer"))
-        .expect("Could not create livestream_buffer element.");
-    music_buffer.set_property("max-size-buffers", &0u32);
-    music_buffer.set_property("max-size-bytes", &0u32);
-    music_buffer.set_property("max-size-time", &(1000_000_000u64));
-
-
-    // Common elements
-    let audio_mixer = gst::ElementFactory::make_with_name("audiomixer", Some("audio_mixer"))
-        .expect("Could not create audio_mixer element.");
-    let mp3_encoder = gst::ElementFactory::make_with_name("lamemp3enc", Some("mp3_encoder"))
-        .expect("Could not create mp3_encoder element.");
-    let stereo_filter = gst::ElementFactory::make_with_name("capsfilter", Some("stereo_filter"))
-        .expect("Could not create stereo_filter element.");
-    let rtsp_sink = gst::ElementFactory::make_with_name("rtspclientsink", Some("rtsp_sink"))
-        .expect("Could not create rtsp_sink element.");
-
-    livestream_cap_filter.set_property("caps", &gst::Caps::builder("audio/x-raw")
-        .field("format", &"S16LE")
-        .field("rate", &44100)
-        .field("channels", &2)
-        .build());
-
-    mp3_encoder.set_property("bitrate", &320);
-    rtsp_sink.set_property("location", &"rtsp://10.0.0.153:8554/sleep");
-    stereo_filter.set_property("caps", &gst::Caps::builder("audio/x-raw").field("channels", &2).build());
-
-    // Add elements to the pipeline
-    pipeline.add_many(&[
-        &livestream_source, &livestream_depay, &livestream_parse, &livestream_decoder, &livestream_queue, &livestream_convert, &livestream_resample, &livestream_dsp, &livestream_buffer,
-        &livestream_cap_convert, &livestream_cap_resample,
-        &livestream_cap_filter,
-        &music_source, &music_cap_filter, &music_volume,
-        &livestream_rgvolume,&music_buffer,
-        &audio_mixer, &stereo_filter, &mp3_encoder, &rtsp_sink
-    ])?;
-
-    // Link static elements for the livestream source
-    gst::Element::link_many(&[
-        &livestream_depay, &livestream_parse, &livestream_decoder
-    ])?;
-
-    gst::Element::link_many(&[
-        &livestream_queue,
-        &livestream_convert,
-        &livestream_resample,
-        &livestream_rgvolume,
-        &livestream_dsp,
-        &livestream_buffer,
-        &livestream_cap_convert,
-        &livestream_cap_resample,
-        &livestream_cap_filter,
-        &audio_mixer,
-    ])?;
-    gst::Element::link_many(&[
-        &music_source, &music_cap_filter, &music_buffer, &music_volume, &audio_mixer
-    ])?;
-
-    gst::Element::link_many(&[
-        &audio_mixer, &stereo_filter, &mp3_encoder, &rtsp_sink
-    ])?;
-
-    // Connect to the pad-added signal of the livestream_source element to dynamically link its source pad
-    livestream_source.connect_pad_added(move |_src, src_pad| {
-        let src_pad_caps = src_pad.current_caps().unwrap();
-        let src_pad_structure = src_pad_caps.structure(0).unwrap();
-
-        if let Ok(media_type) = src_pad_structure.get::<&str>("media") {
-            if media_type == "audio" {
-                let sink_pad = livestream_depay.static_pad("sink").expect("Failed to get sink pad");
-                if let Err(err) = src_pad.link(&sink_pad) {
-                    eprintln!("Failed to link livestream_source audio: {}", err);
-                }
-            }
-        }
-    });
-
-    livestream_decoder.connect_pad_added(move |_, src_pad| {
-        let livestream_queue_sink_pad = livestream_queue.static_pad("sink").expect("Failed to get sink pad from livestream_queue");
-
-        if let Err(err) = src_pad.link(&livestream_queue_sink_pad) {
-            eprintln!("Failed to link livestream_decoder to livestream_queue: {}", err);
-        }
-    });
-
-    pipeline.set_state(gst::State::Playing)?;
+    gst_pipeline.set_state(gst::State::Playing)?;
 
     // Use a Tokio task to manage the GStreamer bus messages asynchronously
-    let bus = pipeline.bus().unwrap();
-    let pipeline_clone = pipeline.clone();
+    let bus = gst_pipeline
+        .get_bus()
+        .expect("Pipeline without bus. Shouldn't happen!");
+
+    let pipeline_clone = gst_pipeline.pipeline.clone();
 
     tokio::spawn(async move {
         for msg in bus.iter_timed(gst::ClockTime::NONE) {
@@ -176,7 +34,11 @@ async fn main() -> Result<(), Error> {
                     break;
                 }
                 gst::MessageView::Error(err) => {
-                    eprintln!("Error from {}: {}", err.src().unwrap().path_string(), err.error());
+                    eprintln!(
+                        "Error from {}: {}",
+                        err.src().map(|s| s.path_string()).unwrap_or_else(|| "None".into()),
+                        err.error()
+                    );
                     break;
                 }
                 _ => (),
@@ -187,14 +49,14 @@ async fn main() -> Result<(), Error> {
         pipeline_clone.set_state(gst::State::Null).unwrap();
     });
 
-    let livestream_volume = Arc::new(Mutex::new(music_volume));
-    let livestream_volume_clone = Arc::clone(&livestream_volume);
+    let music_volume = gst_pipeline.music.volume.clone();
+    let music_volume = Arc::new(Mutex::new(music_volume));
 
     let control_route = warp::path("sleep")
         .and(warp::post())
         .and(warp::body::json())
-        .and_then(move |body: serde_json::Value| {
-            let livestream_volume_clone = Arc::clone(&livestream_volume_clone);
+        .and_then(move |body: Value| {
+            let music_volume_clone = Arc::clone(&music_volume);
             async move {
                 if let Some(time) = body.get("timer") {
                     if let Some(sleep_timer) = time.as_u64() {
@@ -205,7 +67,7 @@ async fn main() -> Result<(), Error> {
                         }
 
                         // Spawn a new task to handle the timer and volume reduction
-                        let livestream_volume_clone = Arc::clone(&livestream_volume_clone);
+                        let music_volume_clone = Arc::clone(&music_volume_clone);
                         tokio::spawn(async move {
                             println!("Starting sleep timer in {} seconds", sleep_timer);
 
@@ -219,8 +81,8 @@ async fn main() -> Result<(), Error> {
                             for step in 0..=100 {
                                 let volume_level = 1.0 - (step as f64 * 0.01);
                                 {
-                                    let livestream_volume = livestream_volume_clone.lock().unwrap();
-                                    livestream_volume.set_property("volume", volume_level);
+                                    let music_volume = music_volume_clone.lock().unwrap();
+                                    music_volume.set_property("volume", volume_level);
                                 }
                                 interval.tick().await;
                             }
@@ -228,15 +90,16 @@ async fn main() -> Result<(), Error> {
                             // Wait for 1 second before executing the bash script
                             time::sleep(Duration::from_secs(1)).await;
                             println!("Executing bash script to pause Spotify");
-                            if let Err(e) = Command::new("/home/wesley/pausespot.sh").status().await {
+                            if let Err(e) = Command::new("/home/wesley/pausespot.sh").status().await
+                            {
                                 eprintln!("Failed to execute bash script: {}", e);
                             }
 
-                            // Wait for 1 second before restoring the volume back to 1.0
+                            // Wait for 5 seconds before restoring the volume back to 1.0
                             time::sleep(Duration::from_secs(5)).await;
                             {
-                                let livestream_volume = livestream_volume_clone.lock().unwrap();
-                                livestream_volume.set_property("volume", 1.0);
+                                let music_volume = music_volume_clone.lock().unwrap();
+                                music_volume.set_property("volume", 1.0);
                             }
 
                             println!("Volume restored");
@@ -261,8 +124,10 @@ async fn main() -> Result<(), Error> {
         warp::serve(routes).run(([0, 0, 0, 0], 7755)).await;
     });
 
-    // // Keep the runtime alive (you could also use some other async logic here)
-    tokio::signal::ctrl_c().await.expect("Failed to listen for ctrl-c");
+    // Keep the runtime alive
+    tokio::signal::ctrl_c()
+        .await
+        .expect("Failed to listen for ctrl-c");
 
     Ok(())
 }
---
+++ src/pipeline.rs
@@ -0,0 +1,326 @@
+use anyhow::Error;
+use gstreamer::prelude::{ElementExt, ElementExtManual, GObjectExtManualGst, GstBinExtManual, ObjectExt, PadExt};
+use gstreamer::{init, Bus, Caps, Element, ElementFactory, Pipeline, State, StateChangeSuccess};
+use gstreamer_rtsp::RTSPLowerTrans;
+
+pub struct StreamPipeline {
+    pub pipeline: Pipeline,
+    pub livestream: LivestreamElements,
+    pub music: MusicElements,
+    pub common: CommonElements,
+}
+
+pub struct MusicElements {
+    source: Element,
+    pub(crate) volume: Element,
+    cap_filter: Element,
+    buffer: Element,
+}
+
+pub struct LivestreamElements {
+    source: Element,
+    depay: Element,
+    parse: Element,
+    decoder: Element,
+    queue: Element,
+    convert: Element,
+    resample: Element,
+    buffer: Element,
+    volume: Element,
+    dsp: Element,
+    cap_filter: Element,
+    cap_resample: Element,
+    cap_convert: Element,
+}
+
+impl LivestreamElements {
+    fn new() -> Result<Self, Error> {
+        let source = ElementFactory::make_with_name("rtspsrc", Some("livestream_source"))
+            .expect("Could not create livestream_source element.");
+        let depay = ElementFactory::make_with_name("rtpmp4gdepay", Some("livestream_depay"))
+            .expect("Could not create livestream_depay element.");
+        let parse = ElementFactory::make_with_name("aacparse", Some("livestream_parse"))
+            .expect("Could not create livestream_parse element.");
+        let decoder = ElementFactory::make_with_name("decodebin", Some("livestream_decoder"))
+            .expect("Could not create livestream_decoder element.");
+        let queue = ElementFactory::make_with_name("queue", Some("livestream_queue"))
+            .expect("Could not create livestream_queue element.");
+        let convert = ElementFactory::make_with_name("audioconvert", Some("livestream_convert"))
+            .expect("Could not create livestream_convert element.");
+        let resample = ElementFactory::make_with_name("audioresample", Some("livestream_resample"))
+            .expect("Could not create livestream_resample element.");
+        let buffer = ElementFactory::make_with_name("queue", Some("livestream_buffer"))
+            .expect("Could not create livestream_buffer element.");
+        let rgvolume = ElementFactory::make_with_name("rgvolume", Some("livestream_rgvolume"))
+            .expect("Could not create livestream_rgvolume element.");
+        let dsp = ElementFactory::make_with_name("webrtcdsp", Some("livestream_dsp"))
+            .expect("Could not create livestream_dsp element.");
+        let cap_filter = ElementFactory::make_with_name("capsfilter", Some("livestream_cap_filter"))
+            .expect("Failed to create capsfilter");
+        let cap_resample = ElementFactory::make_with_name("audioresample", Some("livestream_cap_resample"))
+            .expect("Could not create audioresample element for capsfilter");
+        let cap_convert = ElementFactory::make_with_name("audioconvert", Some("capsfilter_converter"))
+            .expect("Could not create audioconvert element for capsfilter");
+
+        // Set properties
+        source.set_property("location", &"rtsp://10.0.0.12:8554/camera.rlc_520a_clear");
+        source.set_property("protocols", RTSPLowerTrans::TCP);
+
+        dsp.set_property("echo-cancel", &false);
+        dsp.set_property("noise-suppression", &true);
+        dsp.set_property_from_str("noise-suppression-level", "high");
+        dsp.set_property("voice-detection", &true);
+        dsp.set_property("extended-filter", &true);
+
+        // Reduce volume
+        rgvolume.set_property("pre-amp", &-30.0f64);
+
+        buffer.set_property("max-size-buffers", &0u32);
+        buffer.set_property("max-size-bytes", &0u32);
+        buffer.set_property("max-size-time", &(300_000_000u64));
+
+        cap_filter.set_property(
+            "caps",
+            &Caps::builder("audio/x-raw")
+                .field("format", &"S16LE")
+                .field("rate", &44100)
+                .field("channels", &2)
+                .build(),
+        );
+
+        Ok(Self {
+            source,
+            depay,
+            parse,
+            decoder,
+            queue,
+            convert,
+            resample,
+            buffer,
+            volume: rgvolume,
+            dsp,
+            cap_filter,
+            cap_resample,
+            cap_convert,
+        })
+    }
+
+    fn add_to_pipeline(&self, pipeline: &Pipeline) -> Result<(), Error> {
+        pipeline.add_many(&[
+            &self.source,
+            &self.depay,
+            &self.parse,
+            &self.decoder,
+            &self.queue,
+            &self.convert,
+            &self.resample,
+            &self.volume,
+            &self.dsp,
+            &self.buffer,
+            &self.cap_convert,
+            &self.cap_resample,
+            &self.cap_filter,
+        ])?;
+        Ok(())
+    }
+
+    fn link_elements(&self) -> Result<(), Error> {
+        Element::link_many(&[&self.depay, &self.parse, &self.decoder])?;
+        Element::link_many(&[
+            &self.queue,
+            &self.convert,
+            &self.resample,
+            &self.volume,
+            &self.dsp,
+            &self.buffer,
+            &self.cap_convert,
+            &self.cap_resample,
+            &self.cap_filter,
+        ])?;
+        Ok(())
+    }
+}
+
+impl MusicElements {
+    fn new() -> Result<Self, Error> {
+        let source = ElementFactory::make_with_name("pulsesrc", Some("music_source"))
+            .expect("Could not create music_source element.");
+        let volume = ElementFactory::make_with_name("volume", Some("livestream_volume"))
+            .expect("Could not create livestream_volume element.");
+        let cap_filter = ElementFactory::make_with_name("capsfilter", Some("music_cap_filter"))
+            .expect("Failed to create music_cap_filter");
+        let buffer = ElementFactory::make_with_name("queue", Some("music_buffer"))
+            .expect("Could not create livestream_buffer element.");
+
+        // Set properties
+        source.set_property(
+            "device",
+            &"alsa_output.platform-bcm2835_audio.analog-stereo.monitor",
+        );
+        volume.set_property("volume", 1.0f64);
+        cap_filter.set_property(
+            "caps",
+            &Caps::builder("audio/x-raw")
+                .field("format", &"S16LE")
+                .field("rate", &44100)
+                .field("channels", &2)
+                .build(),
+        );
+
+        buffer.set_property("max-size-buffers", &0u32);
+        buffer.set_property("max-size-bytes", &0u32);
+        buffer.set_property("max-size-time", &(750_000_000u64));
+
+        Ok(Self {
+            source,
+            volume,
+            cap_filter,
+            buffer,
+        })
+    }
+
+    fn add_to_pipeline(&self, pipeline: &Pipeline) -> Result<(), Error> {
+        pipeline.add_many(&[&self.source, &self.cap_filter, &self.buffer, &self.volume])?;
+        Ok(())
+    }
+
+    fn link_elements(&self) -> Result<(), Error> {
+        Element::link_many(&[&self.source, &self.cap_filter, &self.buffer, &self.volume])?;
+        Ok(())
+    }
+}
+
+pub struct CommonElements {
+    audio_mixer: Element,
+    mp3_encoder: Element,
+    stereo_filter: Element,
+    rtsp_sink: Element,
+}
+
+impl CommonElements {
+    fn new() -> Result<Self, Error> {
+        let audio_mixer = ElementFactory::make_with_name("audiomixer", Some("audio_mixer"))
+            .expect("Could not create audio_mixer element.");
+        let mp3_encoder = ElementFactory::make_with_name("lamemp3enc", Some("mp3_encoder"))
+            .expect("Could not create mp3_encoder element.");
+        let stereo_filter = ElementFactory::make_with_name("capsfilter", Some("stereo_filter"))
+            .expect("Could not create stereo_filter element.");
+        let rtsp_sink = ElementFactory::make_with_name("rtspclientsink", Some("rtsp_sink"))
+            .expect("Could not create rtsp_sink element.");
+
+        mp3_encoder.set_property("bitrate", &320);
+        rtsp_sink.set_property("location", &"rtsp://10.0.0.153:8554/sleep");
+        stereo_filter.set_property(
+            "caps",
+            &Caps::builder("audio/x-raw").field("channels", &2).build(),
+        );
+
+        Ok(Self {
+            audio_mixer,
+            mp3_encoder,
+            stereo_filter,
+            rtsp_sink,
+        })
+    }
+
+    fn add_to_pipeline(&self, pipeline: &Pipeline) -> Result<(), Error> {
+        pipeline.add_many(&[
+            &self.audio_mixer,
+            &self.mp3_encoder,
+            &self.stereo_filter,
+            &self.rtsp_sink,
+        ])?;
+        Ok(())
+    }
+
+    fn link_elements(&self) -> Result<(), Error> {
+        Element::link_many(&[
+            &self.audio_mixer,
+            &self.stereo_filter,
+            &self.mp3_encoder,
+            &self.rtsp_sink,
+        ])?;
+        Ok(())
+    }
+}
+
+impl StreamPipeline {
+    pub fn new() -> Result<Self, Error> {
+        init()?;
+
+        let pipeline = Pipeline::new();
+
+        let livestream = LivestreamElements::new()?;
+        let music = MusicElements::new()?;
+        let common = CommonElements::new()?;
+
+        livestream.add_to_pipeline(&pipeline)?;
+        music.add_to_pipeline(&pipeline)?;
+        common.add_to_pipeline(&pipeline)?;
+
+        livestream.link_elements()?;
+        music.link_elements()?;
+        common.link_elements()?;
+
+        livestream
+            .cap_filter
+            .link(&common.audio_mixer)
+            .expect("Failed to link livestream to audio mixer");
+
+        music
+            .volume
+            .link(&common.audio_mixer)
+            .expect("Failed to link music to audio mixer");
+
+        Self::connect_dynamic_pads(&livestream)?;
+
+        Ok(Self {
+            pipeline,
+            livestream,
+            music,
+            common,
+        })
+    }
+
+    fn connect_dynamic_pads(livestream: &LivestreamElements) -> Result<(), Error> {
+        // Clone elements for closure
+        let depay_clone = livestream.depay.clone();
+        livestream.source.connect_pad_added(move |_src, src_pad| {
+            let src_pad_caps = src_pad.current_caps().unwrap();
+            let src_pad_structure = src_pad_caps.structure(0).unwrap();
+
+            if let Ok(media_type) = src_pad_structure.get::<&str>("media") {
+                if media_type == "audio" {
+                    let sink_pad = depay_clone
+                        .static_pad("sink")
+                        .expect("Failed to get sink pad");
+                    if let Err(err) = src_pad.link(&sink_pad) {
+                        eprintln!("Failed to link livestream_source audio: {}", err);
+                    }
+                }
+            }
+        });
+
+        let queue_clone = livestream.queue.clone();
+        livestream.decoder.connect_pad_added(move |_, src_pad| {
+            let sink_pad = queue_clone
+                .static_pad("sink")
+                .expect("Failed to get sink pad from livestream_queue");
+
+            if let Err(err) = src_pad.link(&sink_pad) {
+                eprintln!("Failed to link livestream_decoder to livestream_queue: {}", err);
+            }
+        });
+
+        Ok(())
+    }
+
+    pub fn set_state(&self, state: State) -> Result<StateChangeSuccess, Error> {
+        self.pipeline.set_state(state)?;
+        Ok(StateChangeSuccess::Success)
+    }
+
+    pub fn get_bus(&self) -> Option<Bus> {
+        self.pipeline.bus()
+    }
+}
