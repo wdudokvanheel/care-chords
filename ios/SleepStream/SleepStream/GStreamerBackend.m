edf0bd0c5ba86ee5cddc7dc6ef6a9cf0a4bbf994
wesley <wesley@bitechular.com>
2024-11-05 15:54:05 +0100

    Covnert the pipeline to separate elements

--- ios/SleepStream/SleepStream/GStreamerBackend.m
+++ ios/SleepStream/SleepStream/GStreamerBackend.m
@@ -1,20 +1,17 @@
-#include<unistd.h>
-#include "GStreamerBackend.h"
-# include "gst_ios_init.h"
-#include <UIKit/UIKit.h>
+#import <unistd.h>
+#import "GStreamerBackend.h"
+#import "gst_ios_init.h"
+#import <UIKit/UIKit.h>
 
-#include <GStreamer/gst/gst.h>
-#include <GStreamer/gst/video/video.h>
+#import <GStreamer/gst/gst.h>
+#import <GStreamer/gst/video/video.h>
+#import <GStreamer/gst/rtsp/rtsp.h>
 #import "SleepStream-Bridging-Header.h"
 
 GST_DEBUG_CATEGORY_STATIC (debug_category);
 #define GST_CAT_DEFAULT debug_category
 
 #import "SleepStream-Swift.h"
-
-//@interface GStreamerBackendProtocol
-//
-//@end
 
 @interface GStreamerBackend()
 -(void)setUIMessage:(gchar*) message;
@@ -24,14 +21,22 @@ GST_DEBUG_CATEGORY_STATIC (debug_category);
 
 @implementation GStreamerBackend {
     id<GStreamerBackendProtocol> ui_delegate;        /* Class that we use to interact with the user interface */
-    GstElement *pipeline;  /* The running pipeline */
-    GstElement *video_sink;/* The video sink element which receives XOverlay commands */
-    GMainContext *context; /* GLib context used to run the main loop */
-    GMainLoop *main_loop;  /* GLib main loop */
-    gboolean initialized;  /* To avoid informing the UI multiple times about the initialization */
+    GstElement *pipeline;      /* The running pipeline */
+    GstElement *video_sink;    /* The video sink element which receives XOverlay commands */
+    GMainContext *context;     /* GLib context used to run the main loop */
+    GMainLoop *main_loop;      /* GLib main loop */
+    gboolean initialized;      /* To avoid informing the UI multiple times about the initialization */
     GstBus *bus;
-    UIView *ui_video_view; /* UIView that holds the video */
+    UIView *ui_video_view;     /* UIView that holds the video */
     GstMessage* eos_msg;
+
+    /* New elements */
+    GstElement *rtspsrc;
+    GstElement *rtph264depay;
+    GstElement *queue;
+    GstElement *h264parse;
+    GstElement *avdec_h264;
+    GstElement *autovideosink;
 }
 
 /*
@@ -44,11 +49,11 @@ GST_DEBUG_CATEGORY_STATIC (debug_category);
     {
         self->ui_delegate = (id<GStreamerBackendProtocol>)uiDelegate;
         self->ui_video_view = video_view;
-        
+
         GST_DEBUG_CATEGORY_INIT (debug_category, "SleepStreamer", 0, "SleepStreamer-Backend");
         gst_debug_set_threshold_for_name("SleepStreamer", GST_LEVEL_TRACE);
     }
-    
+
     return self;
 }
 
@@ -58,15 +63,12 @@ GST_DEBUG_CATEGORY_STATIC (debug_category);
     return;
 }
 
-
-
 -(void) play
 {
     if(gst_element_set_state(pipeline, GST_STATE_PLAYING) == GST_STATE_CHANGE_FAILURE) {
         [self setUIMessage:"Failed to set pipeline to playing"];
     }
 }
-
 
 -(void) pause
 {
@@ -84,11 +86,10 @@ GST_DEBUG_CATEGORY_STATIC (debug_category);
     gst_element_post_message (pipeline, eos_msg);
 }
 
-
 /* Change the message on the UI through the UI delegate */
 -(void)setUIMessage:(gchar*) message
 {
-    printf("Setting message to: {} %s", message);
+    printf("Setting message to: %s\n", message);
     NSString *messagString = [NSString stringWithUTF8String:message];
     if(ui_delegate)
     {
@@ -97,7 +98,7 @@ GST_DEBUG_CATEGORY_STATIC (debug_category);
 }
 
 static void eos_cb(GstBus *bus, GstMessage *msg, GStreamerBackend *self){
-    printf("\neos called\n");
+    printf("\nEOS called\n");
     gst_element_set_state (self->pipeline, GST_STATE_NULL);
     g_main_loop_quit(self->main_loop);
 }
@@ -108,10 +109,10 @@ static void error_cb (GstBus *bus, GstMessage *msg, GStreamerBackend *self)
     GError *err;
     gchar *debug_info;
     gchar *message_string;
-    
+
     gst_message_parse_error (msg, &err, &debug_info);
     message_string = g_strdup_printf ("Error received from element %s: %s", GST_OBJECT_NAME (msg->src), err->message);
-    printf("Some error occured in from element %s: %s", GST_OBJECT_NAME (msg->src), err->message);
+    printf("Error from element %s: %s\n", GST_OBJECT_NAME (msg->src), err->message);
     g_clear_error (&err);
     g_free (debug_info);
     [self setUIMessage:message_string];
@@ -124,15 +125,15 @@ static void state_changed_cb (GstBus *bus, GstMessage *msg, GStreamerBackend *se
 {
     GstState old_state, new_state, pending_state;
     gst_message_parse_state_changed (msg, &old_state, &new_state, &pending_state);
-    
+
     /* Only pay attention to messages coming from the pipeline, not its children */
     if (GST_MESSAGE_SRC (msg) == GST_OBJECT (self->pipeline)) {
-        printf(g_strdup_printf("State changed from %s to %s\n", gst_element_state_get_name(old_state), gst_element_state_get_name(new_state)));
+        printf("State changed from %s to %s\n", gst_element_state_get_name(old_state), gst_element_state_get_name(new_state));
 
         if (new_state == GST_STATE_READY) {
             [self play];
         }
-        
+
         gchar *message = g_strdup_printf("State changed from %s to %s", gst_element_state_get_name(old_state), gst_element_state_get_name(new_state));
         [self setUIMessage:message];
         g_free (message);
@@ -153,40 +154,107 @@ static void state_changed_cb (GstBus *bus, GstMessage *msg, GStreamerBackend *se
     }
 }
 
+static void on_pad_added(GstElement *src, GstPad *new_pad, GStreamerBackend *self)
+{
+    GstCaps *caps;
+    GstStructure *str;
+    const gchar *new_pad_type;
+
+    /* Check the new pad's type */
+    caps = gst_pad_get_current_caps(new_pad);
+    if (!caps) {
+        caps = gst_pad_query_caps(new_pad, NULL);
+    }
+    str = gst_caps_get_structure(caps, 0);
+    new_pad_type = gst_structure_get_name(str);
+
+    printf("Received new pad '%s' from '%s':\n", new_pad_type, GST_ELEMENT_NAME(src));
+
+    if (g_str_has_prefix(new_pad_type, "application/x-rtp")) {
+        /* Check if it's video */
+        const gchar *media = gst_structure_get_string(str, "media");
+        if (g_strcmp0(media, "video") == 0) {
+            GstPad *sink_pad = gst_element_get_static_pad(self->rtph264depay, "sink");
+            GstPadLinkReturn ret;
+
+            /* Attempt to link the dynamic pad to rtph264depay sink pad */
+            ret = gst_pad_link(new_pad, sink_pad);
+            if (GST_PAD_LINK_FAILED(ret)) {
+                gchar *message = g_strdup_printf("Failed to link dynamic video pad.");
+                [self setUIMessage:message];
+                g_free(message);
+            } else {
+                GST_DEBUG("Link succeeded (video).");
+            }
+            gst_object_unref(sink_pad);
+        } else if (g_strcmp0(media, "audio") == 0) {
+            printf("Ignoring audio pad.\n");
+        }
+    } else {
+        printf("Unknown pad type: %s\n", new_pad_type);
+    }
+
+    gst_caps_unref(caps);
+}
+
 /* Main method */
 -(void) run_app_pipeline
 {
     GSource *bus_source;
-    GError *error = NULL;
-    
     GST_DEBUG ("Creating pipeline");
-    
+
     /* Create our own GLib Main Context and make it the default one */
     context = g_main_context_new ();
     g_main_context_push_thread_default(context);
-    
-    /* Build pipeline */
-    /* Change the RTSP URL to your desired URL below */
-    pipeline = gst_parse_launch("rtspsrc location=rtsp://10.0.0.12:8554/camera.rlc_520a_clear protocols=tcp ! rtph264depay ! queue ! h264parse !  avdec_h264 ! autovideosink", &error);
-    
-    if (error && !GST_IS_ELEMENT(pipeline)) {
-        gchar *message = g_strdup_printf("Unable to build pipeline: %s", error->message);
-        g_clear_error (&error);
+
+    /* Create the pipeline and elements */
+    pipeline = gst_pipeline_new("pipeline");
+    self->pipeline = pipeline;
+
+    self->rtspsrc = gst_element_factory_make("rtspsrc", "source");
+    self->rtph264depay = gst_element_factory_make("rtph264depay", "depay");
+    self->queue = gst_element_factory_make("queue", "queue");
+    self->h264parse = gst_element_factory_make("h264parse", "parse");
+    self->avdec_h264 = gst_element_factory_make("avdec_h264", "decoder");
+    self->autovideosink = gst_element_factory_make("autovideosink", "videosink");
+
+    if (!pipeline || !self->rtspsrc || !self->rtph264depay || !self->queue || !self->h264parse || !self->avdec_h264 || !self->autovideosink) {
+        gchar *message = g_strdup_printf("Not all elements could be created.");
         [self setUIMessage:message];
-        g_free (message);
+        g_free(message);
         return;
     }
-    
+
+    /* Set element properties */
+    g_object_set(self->rtspsrc, "location", "rtsp://10.0.0.12:8554/camera.rlc_520a_clear", NULL);
+    g_object_set(self->rtspsrc, "protocols", GST_RTSP_LOWER_TRANS_TCP, NULL);
+
+    /* Add elements to the pipeline */
+    gst_bin_add_many(GST_BIN(pipeline), self->rtspsrc, self->rtph264depay, self->queue, self->h264parse, self->avdec_h264, self->autovideosink, NULL);
+
+    /* Link the elements (except rtspsrc, which is linked dynamically) */
+    if (!gst_element_link_many(self->rtph264depay, self->queue, self->h264parse, self->avdec_h264, self->autovideosink, NULL)) {
+        gchar *message = g_strdup_printf("Elements could not be linked.");
+        [self setUIMessage:message];
+        g_free(message);
+        gst_object_unref(pipeline);
+        return;
+    }
+
+    /* Connect to the pad-added signal for dynamic pad linking */
+    g_signal_connect(self->rtspsrc, "pad-added", G_CALLBACK(on_pad_added), (__bridge void *)self);
+
     /* Set the pipeline to READY, so it can already accept a window handle */
     gst_element_set_state(pipeline, GST_STATE_READY);
-    
-    video_sink = gst_bin_get_by_interface(GST_BIN(pipeline), GST_TYPE_VIDEO_OVERLAY);
-    if (!video_sink) {
+
+    /* Set the video sink */
+    self->video_sink = gst_bin_get_by_interface(GST_BIN(pipeline), GST_TYPE_VIDEO_OVERLAY);
+    if (!self->video_sink) {
         GST_ERROR ("Could not retrieve video sink");
         return;
     }
-    gst_video_overlay_set_window_handle(GST_VIDEO_OVERLAY(video_sink), (guintptr) (id) ui_video_view);
-    
+    gst_video_overlay_set_window_handle(GST_VIDEO_OVERLAY(self->video_sink), (guintptr) (id) ui_video_view);
+
     /* Signals to watch */
     bus = gst_element_get_bus (pipeline);
     bus_source = gst_bus_create_watch (bus);
@@ -197,26 +265,22 @@ static void state_changed_cb (GstBus *bus, GstMessage *msg, GStreamerBackend *se
     g_signal_connect (G_OBJECT (bus), "message::eos", (GCallback)eos_cb, (__bridge void *)self);
     g_signal_connect (G_OBJECT (bus), "message::state-changed", (GCallback)state_changed_cb, (__bridge void *)self);
     gst_object_unref (bus);
-    
+
     /* Create a GLib Main Loop and set it to run */
-//    GST_DEBUG ("Entering main loop...");
-    printf("\nEntering main loop..\n");
+    printf("\nEntering main loop...\n");
     main_loop = g_main_loop_new (context, FALSE);
-    //sleep(5);
     [self check_initialization_complete];
     g_main_loop_run (main_loop);
     GST_DEBUG ("Exited main loop");
     g_main_loop_unref (main_loop);
     main_loop = NULL;
-    
+
     /* Free resources */
     g_main_context_pop_thread_default(context);
     g_main_context_unref (context);
     gst_element_set_state (pipeline, GST_STATE_NULL);
-    gst_object_unref (pipeline);    
+    gst_object_unref (pipeline);
     return;
 }
 
 @end
-
-
--- ios/SleepStream/SleepStream/SleepStreamApp.swift
+++ ios/SleepStream/SleepStream/SleepStreamApp.swift
@@ -3,6 +3,9 @@ import SwiftUI
 @main
 struct SleepStreamApp: App {
     init() {
+        setenv("GST_DEBUG", "4", 1)
+        setenv("GST_DEBUG_NO_COLOR", "1", 1)
+
         print("Init gst")
         gst_ios_init()
         print("Init gst done")
